e8c2e08994ad342b1dc676a976fc5ad7bd04e3ee
githubsah <129147915+githubsah@users.noreply.github.com>
2026-02-08 18:58:13 +0530

    Delete Q4b_total_cogs.sql

--- Q4b_total_cogs.sql
+++
@@ -1,8 +0,0 @@
--- =====================================================
--- Q4b: Total Cost of Goods Sold (COGS)
--- Objective: Calculate total cost incurred to fulfill orders
--- =====================================================
-
-SELECT
-    ROUND(SUM(cogs_usd), 2) AS total_cogs
-FROM order_items;
